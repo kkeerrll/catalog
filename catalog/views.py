ec29fc811d1ff8c10107163d224c97ebaa20191e
anzelikagudkova <kkeerrll137@gmail.com>
2023-09-28 10:21:16 +0300

    hw

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
 
 def home(request):
     return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"Имя: {name}, Номер телефона: {phone}, Сообщение: {message}")
+    return render(request, 'catalog/contacts.html')
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,14 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import include
-from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', include('catalog.urls')),
+    path('', include('catalog.urls'))
 ]
-
-
-
